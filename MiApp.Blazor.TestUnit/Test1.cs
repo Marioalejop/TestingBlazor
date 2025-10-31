b79e363b21d200cfe8860d655af25b964c4e5b94
unknown <marioalejop@gmial.com>
2025-10-30 21:53:15 -0500

    app blazor

---
+++ MiApp.Blazor.TestUnit/Test1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiApp.Blazor.Pages;
+
+namespace MiApp.Blazor.TestUnit
+{
+    [TestClass]
+    public class TareasTests
+    {
+        [TestMethod]
+        public void AgregarTarea_DeberiaAumentarLaListaYActualizarMensaje()
+        {
+            // Arrange
+            var pagina = new Tareas();
+
+            // Act
+            pagina.AgregarTarea();
+
+            // Assert
+            Assert.AreEqual(1, pagina.listaTareas.Count);
+            Assert.AreEqual("Total tareas: 1", pagina.mensaje);
+        }
+    }
+}
