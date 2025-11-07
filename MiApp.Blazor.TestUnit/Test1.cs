435647d2ea4b816f3257c5b401bc9a0904aa1a3c
unknown <marioalejop@gmial.com>
2025-11-07 12:20:28 -0500

    test1

--- MiApp.Blazor.TestUnit/Test1.cs
+++ MiApp.Blazor.TestUnit/Test1.cs
@@ -1,17 +1,34 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MiApp.Blazor.Pages;
+using System.Collections.Generic;
 
 namespace MiApp.Blazor.TestUnit
 {
     [TestClass]
     public class TareasTests
     {
+        private List<string> listaTareas;
+        private string mensaje;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            listaTareas = new List<string>();
+            mensaje = string.Empty;
+        }
+
         [TestMethod]
         public void AgregarTarea_DeberiaAumentarLaListaYActualizarMensaje()
         {
-            var componente = new Tareas();
-            componente.AgregarTarea();
-            Assert.AreEqual(1, componente.listaTareas.Count);
+            // Arrange
+            var tarea = "Nueva tarea";
+
+            // Act
+            listaTareas.Add(tarea);
+            mensaje = $"Total tareas: {listaTareas.Count}";
+
+            // Assert
+            Assert.AreEqual(1, listaTareas.Count, "La lista debería tener una tarea.");
+            Assert.AreEqual("Total tareas: 1", mensaje, "El mensaje debería reflejar el total.");
         }
     }
 }
